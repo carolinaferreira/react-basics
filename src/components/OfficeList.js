5da4d7ccd7f3ee1f15290bc8dfe081e6c0a6b58a
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-24 20:20:16 -0300

    added state to show the input text

--- src/components/OfficeList.js
+++ src/components/OfficeList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class OfficeList extends Component {
   state={
+    newOfficer: '',
     officers: [
       'Angela',
       'Dwight',
@@ -10,11 +11,19 @@ class OfficeList extends Component {
       'Pam',
     ]
   }
+  handleInputChange = e =>{
+    this.setState({ newOfficer: e.target.value})
+  }
+
   render(){
     return (
+    <>
+      <h1>{this.state.newOfficer}</h1>
       <ul>
         {this.state.officers.map(office => <li key={office}>{office}</li>)}
       </ul>
+      <input type="text" onChange={this.handleInputChange} />
+    </>
     )
   }
 }
