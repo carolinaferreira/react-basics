6aa6a4592e9981db1d002af9c9a32555df8bd738
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-26 18:38:58 -0300

    added prop types

--- src/components/OfficerItem.js
+++ src/components/OfficerItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function OfficerItem({ officer, onDelete }){
   return (
@@ -9,4 +10,8 @@ function OfficerItem({ officer, onDelete }){
   );
 }
 
+OfficerItem.PropTypes ={
+  officer: PropTypes.string,
+  onDelete: PropTypes.func.isRequired,
+}
 export default OfficerItem;
